3ad3f62b0508494a8a34d298a44cf7687600d60a
bzlister <bzlister@gmail.com>
2018-03-13 10:28:03 -0400

    Fixed error in histograms.py resulting in bin votes of alpha*alpha instead of alpha*theta. Added more functionality to linearDA.py

--- histograms.py
+++ histograms.py
@@ -8,32 +8,25 @@ def statistics(angles):
     alphaMean = [0]*10
     thetaMean = [0]*10
     for i in range(0, 10):
-        actualFrames = 0
         alphaActionSum = [0]*9
         thetaActionSum = [0]*9
         for j in range(0, len(angles[i])):
-            if (angles[i][j] != 0):
-                actualFrames+=1
-                for k in range(0, 9):
-                    alphaActionSum[k]+=angles[i][j][k][0]
-                    thetaActionSum[k]+=angles[i][j][k][1]
-        
-        alphaMean[i] = [(1/actualFrames)*x for x in alphaActionSum]
-        thetaMean[i] = [(1/actualFrames)*x for x in thetaActionSum]
+            for k in range(0, 9):
+                alphaActionSum[k]+=angles[i][j][k][0]
+                thetaActionSum[k]+=angles[i][j][k][1]        
+        alphaMean[i] = [(1/len(angles[i]))*x for x in alphaActionSum]
+        thetaMean[i] = [(1/len(angles[i]))*x for x in thetaActionSum]
     alphaDev = [0]*10
     thetaDev = [0]*10
     for x in range(0, 10):
-        actualFrames = 0
         alphaActionDev = [0]*9
         thetaActionDev = [0]*9
         for y in range(0, len(angles[x])):
-            if (angles[x][y] != 0):
-                actualFrames+=1
-                for z in range(0, 9):
-                    alphaActionDev[z]+=math.pow(angles[x][y][z][0]-alphaMean[x][z], 2)
-                    thetaActionDev[z]+=math.pow(angles[x][y][z][1]-thetaMean[x][z], 2)
-        alphaDev[x] = [math.sqrt((1/actualFrames)*q) for q in alphaActionDev]
-        thetaDev[x] = [math.sqrt((1/actualFrames)*q) for q in thetaActionDev]
+            for z in range(0, 9):
+                alphaActionDev[z]+=math.pow(angles[x][y][z][0]-alphaMean[x][z], 2)
+                thetaActionDev[z]+=math.pow(angles[x][y][z][1]-thetaMean[x][z], 2)
+        alphaDev[x] = [math.sqrt((1/len(angles[x]))*q) for q in alphaActionDev]
+        thetaDev[x] = [math.sqrt((1/len(angles[x]))*q) for q in thetaActionDev]
     return [alphaMean, thetaMean, alphaDev, thetaDev]
 
 #Returns a 100-bin histogram
@@ -42,11 +35,10 @@ def getHisto(joints, alphaMeans, thetaMeans, alphaDevs, thetaDevs):
     bins= [0]*10
     for b in range(0, 10):
         bins[b] = [0]*10
-    
     delta = math.pi/10
     for j in range(0, 9):
         alphaStart = int(joints[j][0]/delta)
-        thetaStart = int(joints[j][0]/delta)
+        thetaStart = int(joints[j][1]/delta)
         for n in range(max(alphaStart-1, 0), min(alphaStart+2, 10)):
             for m in range(max(thetaStart-1, 0), min(thetaStart+2, 10)):
                 alphaVote = st.norm.cdf(((n+1)*delta - alphaMeans[j])/alphaDevs[j]) - st.norm.cdf((n*delta-alphaMeans[j])/alphaDevs[j])
--- linearDA.py
+++ linearDA.py
@@ -5,17 +5,17 @@ import numpy as np
 #Returns an enormous Nx100 feature vector of bin votes and a Nx1 class vector of actions
 #using the entire dataset excepting the test entry and the last (19th) entry, which has missing data
 def setup(test):
-    X = [0]*5426
-    Y = np.zeros(5426)
+    X = []
+    Y = []
     index = 0
     for i in range(0, 19):
-        print(i)
         if (i != test):
             sperson = str(int(i/2)+1)
             strial = "0"+str(i%2+1)
             if (i < 18):
                 sperson = "0"+sperson
             filename = 'joints\\joints_s' + sperson + '_e' + strial + '.txt'
+            print(filename)
             data = sphereicalCoordinates.prepData(sphereicalCoordinates.getData(filename), i)
             stats = histograms.statistics(data)
             #Every action
@@ -24,8 +24,8 @@ def setup(test):
                 for k in range(0, len(data[j])):
                     #Compute a histogram representing a posture
                     histo = histograms.getHisto(data[j][k], stats[0][j], stats[1][j], stats[2][j], stats[3][j])
-                    X[index] = histo
-                    Y[index] = j
+                    X.append(histo)
+                    Y.append(j)
                     index+=1
     return [X, Y]
 
@@ -49,3 +49,26 @@ def meanVectors(X, Y):
             index+=1
         meanVec.append((1/(index-start))*m)
     return meanVec
+
+
+def withinClassScatter(X, Y, meanVec):
+    index = 0
+    S_W = np.zeros((100, 100))
+    for cl, mv in zip(range(0, 10), meanVec):
+        class_sc_mat = np.zeros((100, 100))
+        while (Y[index] == cl):
+            row = X[index].reshape(100, 1)
+            mv = mv.reshape(100, 1)
+            class_sc_mat += (row-mv).dot((row-mv).T)
+            index+=1
+        S_W += class_sc_mat
+    return S_W
+
+def betweenClassScatter(X, Y, meanVec):
+    overall_mean = np.mean(X, axis=0).reshape(100, 1)
+    S_B = np.zeros((100, 100))
+    for i in range(0, 10):
+        n = Y.count(i)
+        m = meanVec[i].reshape(100, 1)
+        S_B += n*(m - overall_mean).dot((m - overall_mean).T)
+    return S_B
